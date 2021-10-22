d0571babf6fb7629a534e90913e074fc2a584189
Swoosky <agushusky@gmail.com>
2021-10-22 22:23:30 +0800

    cleaning file tree

--- app/src/main/java/com/example/tubes_uts_e_2/activity/LoginActivity.java
+++ app/src/main/java/com/example/tubes_uts_e_2/activity/LoginActivity.java
@@ -3,10 +3,13 @@ package com.example.tubes_uts_e_2.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.tubes_uts_e_2.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
