aa025cb507d202e8d6c7144a9120fb23e3d0b740
Bapaktole <swaraiddo@gmail.com>
2021-10-25 13:42:59 +0700

    Add Java NotificationFirebase and update gradle firebase

---
+++ app/src/main/java/com/example/tubes_uts_e_2/activity/NotificationFirebase.java
@@ -0,0 +1,14 @@
+package com.example.tubes_uts_e_2.activity;
+
+import android.app.Application;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class NotificationFirebase extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseMessaging.getInstance().subscribeToTopic("sample_notification");
+    }
+}
