e505f3139f8ac0ef106e7dfbe4ab8c26a855049f
ASUS <elnkabagaskara@gmail.com>
2021-12-20 23:30:32 +0700

    loading

--- app/src/main/java/com/example/tubes_uts_e_2/fragment/ThirdFragment.java
+++ app/src/main/java/com/example/tubes_uts_e_2/fragment/ThirdFragment.java
@@ -25,6 +25,7 @@ import com.example.tubes_uts_e_2.model.Ticket;
 import com.example.tubes_uts_e_2.model.TicketResponse;
 import com.example.tubes_uts_e_2.model.User;
 import com.example.tubes_uts_e_2.preferences.UserPreferences;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import org.json.JSONObject;
 
@@ -41,7 +42,7 @@ import retrofit2.Response;
  * create an instance of this fragment.
  */
 public class ThirdFragment extends Fragment implements CallBackInterface {
-
+    private ShimmerFrameLayout layout;
     private RecyclerView rv_ticket;
     private List<Ticket> ticketList;
     private TicketAdapter ticketAdapter;
@@ -105,7 +106,7 @@ public class ThirdFragment extends Fragment implements CallBackInterface {
 
         apiService = ApiClient.getClient().create(ApiInterface.class);
 //        layoutLoading = view.findViewById(R.id.layout_loading);
-
+        layout = view.findViewById(R.id.shimer);
         rv_ticket = view.findViewById(R.id.ticket_list);
         rv_ticket.setLayoutManager(new LinearLayoutManager(getContext()));
         ticketList = new ArrayList<>();
@@ -114,6 +115,18 @@ public class ThirdFragment extends Fragment implements CallBackInterface {
         user = userPreferences.getUserLogin();
         username = user.getUsername();
         getUserTickets(username);
+
+        //        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                layout.stopShimmer();
+//                layout.hideShimmer();
+//                layout.setVisibility(View.GONE);
+//
+//
+//                rv_ticket.setVisibility(View.VISIBLE);
+//            }
+//        },8000);
     }
 
     private void getTickets() {
